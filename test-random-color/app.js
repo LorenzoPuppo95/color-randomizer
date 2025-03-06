a7e34d4327daffe94119a00c29fa5118253423ad
LorenzoPuppo95 <puppolorenzo@live.it>
2025-03-06 12:50:12 +0100

    add

--- test-random-color/app.js
+++ test-random-color/app.js
@@ -1,23 +1,6 @@
-import {randomColorArray, rgbToHex} from "./utils.js";
+import { randomColorArray, randomColorHex, randomColorRgb, randomColorObj } from '@lorenzopuppo95/random-color';
 
-function randomColorHex() {
-    const rgbArray = randomColorArray();
-    const hexString = rgbToHex(rgbArray);
-    return hexString.toUpperCase();
-}
-
-function randomColorRgb() {
-    const rgbArray = randomColorArray();
-    const rgbString = `rgb(${rgbArray[0]},${rgbArray[1]},${rgbArray[2]})`;
-    return rgbString;
-}
-
-function randomColorObj() {
-    const rgbArray = randomColorArray();
-    const rgbObj = {r: rgbArray[0], g: rgbArray[1], b: rgbArray[2]};
-    return rgbObj;
-}
-// console.log(randomColorArray());
-// console.log(randomColorHex());
-// console.log(randomColorRgb());
-// console.log(randomColorObj());
+console.log(randomColorArray());
+console.log(randomColorHex());
+console.log(randomColorRgb());
+console.log(randomColorObj());
